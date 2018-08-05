18dc36f1aa2cb6fdac4aa5b5e14bc9316846e339
Youxi Li <yol107@ucsd.edu>
2018-08-04 17:00:32 -0700

    added routers without database

--- server/index.js
+++ server/index.js
@@ -1,41 +1,121 @@
-const express = require('express');
-const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+// Answer API requests.
+app.get("/api", function(req, res) {
+  res.set("Content-Type", "application/json");
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+app.get("/apii", function(req, res) {
+  res.set("Content-Type", "application/json");
+  res.send('{"message":"Hello from the yuki!"}');
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log("here is /user/:id");
+  console.log(req.params.id);
+  if (req.params.id == "initialdefault-firstuser") {
+    var sql = "SELECT * FROM people LIMIT 1";
+  } else {
+    var sql = escape(
+      "SELECT * FROM people where producthunt_id= %L",
+      req.params.id
+    );
   }
+  res.send({ name: "hello????????????/" });
+  /*
+    let resData;
+    client.query(sql, (error, response) => {
+        //console.log(err, res);
+        resData = response.rows[0];
+        res.send(resData);
+    });
+    */
+});
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
+app.get("/users", (req, res) => {
+  var sql = "SELECT * FROM people";
+  res.send({ name: "hello????????????/" });
+  /*
+    let resData;
+    client.query(sql, (error, response) => {
+        //console.log(err, res);
+        resData = response.rows;
+        res.send(resData);
+    });*/
+});
 
-} else {
-  const app = express();
+app.get("/users/:posts", (req, res) => {
+  const filters = req.params.posts.split("&");
+  const uid = "MDQ6VXNlcjI3Mzk5NjU2";
+  res.send([{ name: "hello????????????/" }, { name: "2222222222222" }]);
+  /*
+    let sqlfilter = filters.map(slug => {
+        client.query(
+            escape(
+                "SELECT exists (SELECT 1 FROM posts WHERE slug = %L LIMIT 1)",
+                slug
+            ),
+            (error, response) => {
+                const isSlugNew = !response.rows[0].exists;
+                if (isSlugNew) {
+                    worker.queuePostBySlug(slug, uid);
+                }
+            }
+        );
+        return "'" + slug + "'";
+    });
+    const sql = escape(
+        "SELECT * FROM people WHERE producthunt_id IN (SELECT uid FROM votes WHERE pid IN (SELECT id FROM posts WHERE slug in (%s)));",
+        sqlfilter.toString()
+    );
+    let resData;
+    client.query(sql, (error, response) => {
+        //console.log(err, res);
+        resData = response.rows;
+        res.send(resData);
+    });*/
+});
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.post("/login", (req, res) => {
+  const token = req.body.token;
+  const userid = req.body.userid;
+  console.log("Got you login");
+  console.log(token);
+  console.log(userid);
+  /*
+    const sql = escape(
+        "UPDATE users SET token=%L WHERE userid=%L; INSERT INTO users (userid, token) SELECT %L, %L WHERE NOT EXISTS (SELECT * FROM users WHERE userid= %L);",
+        token,
+        userid,
+        userid,
+        token,
+        userid
+    );
+    client.query(sql, (error, response) => {
+        console.log(error, response);
+    });*/
+});
 
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
-
-  app.listen(PORT, function () {
-    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-  });
-}
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function(request, response) {
+  response.sendFile(path.resolve(__dirname, "../react-ui/build", "index.html"));
+});
+app.listen(PORT, function() {
+  console.error(
+    `Node cluster worker ${process.pid}: listening on port ${PORT}`
+  );
+});
